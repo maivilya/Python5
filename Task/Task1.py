d2dce5e0176ccb3c7f5f0f8bc20bb3feb3538d59
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-01-16 01:04:51 +0500

    delete word with абв

---
+++ Task/Task1.py
@@ -0,0 +1,19 @@
+text = "дядя ваня ехал на метро, он ехал не быстро: примерно 50 к в час. с ним в машине была его собака жучка."
+
+while "," in text or ":" in text or "." in text:
+    text = text.replace(",", "")
+    text = text.replace(":", "")
+    text = text.replace(".", "")
+    break
+
+text_list = text.split()
+print(text_list)
+
+count = 0
+while count < len(text_list):
+    for word in text_list:
+        if "а" in word or "б" in word or "в" in word:
+            text_list.remove(word)
+    count+=1
+
+print(text_list)
